f53279e962f042bae38c942dd2308c3ac2476bca
Ramiro Galende <ramirogalende@gmail.com>
2019-10-01 09:41:03 -0300

    Updated Style

--- Componentes/Main/Main.js
+++ Componentes/Main/Main.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import { NavigationBar } from 'navigationbar-react-native'
+import { Text, View, Image, ScrollView } from 'react-native';
+import { Button } from 'react-native-elements';
+import { NavigationBar } from 'navigationbar-react-native';
 const Administrador = () => {
     return (
         <View style={{flex: 1, width: '100%'}}>
             <NavigationBar 
-                componentLeft = {() => <Text>Prueba</Text>}
+                navigationBarStyle={{backgroundColor:'#9e5abf'}}
+                componentLeft = {() => <Image source={require('../../Images/Untitled.png')} style={{height:'100%'}} resizeMode='contain' />}
             />
-            <View style={{width: '100%', backgroundColor: '#9e5abf'}}>
-            
+            <View style={{width: '100%', backgroundColor: '#9e5abf', flexDirection: 'row', borderTopWidth: 1}}>
+                <ScrollView horizontal>
+                    <Button title='Registrar Lote' buttonStyle={{backgroundColor:'#9e5abf'}}/>
+                    <Button title='Control de Lotes' buttonStyle={{backgroundColor:'#9e5abf'}}/>
+                    <Button title='Usuarios' buttonStyle={{backgroundColor:'#9e5abf'}}/>
+                    <Button title='Informes' buttonStyle={{backgroundColor:'#9e5abf'}}/>
+                    <Button title='Armado de Carro' buttonStyle={{backgroundColor:'#9e5abf'}}/>
+                    <Button title='Armado de Pedido' buttonStyle={{backgroundColor:'#9e5abf'}}/>
+                    <Button title='Despacho' buttonStyle={{backgroundColor:'#9e5abf'}}/>
+                </ScrollView>
             </View>
         </View>
     );
