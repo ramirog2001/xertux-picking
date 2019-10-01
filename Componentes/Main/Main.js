b455640c9539b884abe4ae6a56b38809c0b5df02
Skyz <48739250+SkyzInformatica@users.noreply.github.com>
2019-10-01 11:03:32 -0300

    Update Main.js

--- Componentes/Main/Main.js
+++ Componentes/Main/Main.js
@@ -4,13 +4,16 @@ import { Button } from 'react-native-elements';
 import { NavigationBar } from 'navigationbar-react-native';
 import Buttons from './Buttons';
 import NavigationMaster from './NavigationMaster';
+
+
 const Administrador = () => {
     return (
         <View style={{flex: 1, width: '100%'}}>
             <StatusBar hidden/>
             <NavigationBar 
                 navigationBarStyle={{backgroundColor:'#9e5abf',padding: 5,}}
-                componentLeft = {() => <Image source={require('../../Images/Untitled.png')} style={{height:'100%',width:120,}}  resizeMode='center'/>}
+                componentLeft = {() => <Image source={require('../../Images/Untitled.png')}
+                                        style={{height:'100%',width:120,}}  resizeMode='center'/>}
             />
             <NavigationMaster />
         </View>
