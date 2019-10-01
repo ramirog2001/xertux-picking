d231542cee7ae7f39b2a999856e0b68e12455de6
Ramiro Galende <ramirogalende@gmail.com>
2019-10-01 09:15:41 -0300

    Avances
    Visual

--- Componentes/Main/Main.js
+++ Componentes/Main/Main.js
@@ -4,14 +4,12 @@ import { NavigationBar } from 'navigationbar-react-native'
 const Administrador = () => {
     return (
         <View style={{flex: 1, width: '100%'}}>
+            <NavigationBar 
+                componentLeft = {() => <Text>Prueba</Text>}
+            />
             <View style={{width: '100%', backgroundColor: '#9e5abf'}}>
             
-                <Text style={{fontSize: 30, color: 'white'}}>
-                    X
-                </Text>
             </View>
-            <NavigationBar 
-            />
         </View>
     );
 };
