ed3416d89e1a413b429d57d76790725034386e4c
Ramiro Galende <ramirogalende@gmail.com>
2019-10-04 12:47:09 -0300

    Update NavigationMaster.js

--- Componentes/Main/NavigationMaster.js
+++ Componentes/Main/NavigationMaster.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 //import paginas
-import RegistrarLote from './Administracion/RegistrarLote';
+import RegistrarLote from './Administracion/RegistroLotes/RegistrarLote';
 import ControlLote from './Administracion/ControlDeLote/ControlLote';
 import Informes from './Administracion/Informes';
 import Usuarios from './Administracion/Usuarios/Usuarios';
 
 import ArmadoCarro from './Armado-Despacho/ArmadoDeCarro/ArmadoCarro';
-import ArmadoPedido from './Armado-Despacho/ArmadoPedido';
+import ArmadoPedido from './Armado-Despacho/ArmadoPedido/ArmadoPedido';
 import Despacho from './Armado-Despacho/Despacho/Despacho'
 
 
@@ -21,9 +21,6 @@ const root = createSwitchNavigator({
     ArmadoCarro,
     ArmadoPedido,
     Despacho
-},
-{
-    initialRouteName: 'Usuarios'
 })
 
 export default createAppContainer(root);
