a379b0a3098adda213736f952d640dabe17d55b5
Skyz <48739250+SkyzInformatica@users.noreply.github.com>
2019-10-18 11:18:23 -0300

    Update Notificaciones.js

--- Componentes/Main/Notificaciones.js
+++ Componentes/Main/Notificaciones.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 import {View, Text, StyleSheet} from 'react-native'
+
+import Ionicons from '@expo/vector-icons/Ionicons'
+import { Avatar, Badge, Icon, withBadge } from 'react-native-elements'
+
 class Notificaciones extends Component {
+
+
+    BadgedIcon = withBadge(1)(Icon);
+
     render() {
         return (
-            <View>
-                <Text style={styles.texto}>NOtificaciones</Text>
+            <View style={{flexDirection: 'row', justifyContent:'center',flexWrap:'wrap'}}>
+                <View style={{width:'50%'}}>
+                    <Ionicons
+                        name="ios-person"
+                        size={30}
+                    />
+                </View>
+                <View style={{width:'50%'}}>
+                
+                <BadgedIcon type="ionicon" name="ios-chatbubbles" />
+                </View>
             </View>
         );
     }
