2b88409a330d6cd683c9425b83bd48466edef7ed
Skyz <48739250+SkyzInformatica@users.noreply.github.com>
2019-10-18 12:33:08 -0300

    gf

--- App.js
+++ App.js
@@ -7,7 +7,6 @@ import {createStackNavigator} from 'react-navigation-stack'
 import {createDrawerNavigator} from 'react-navigation-drawer'
 
 //Componentes
-<<<<<<< Updated upstream
 import LoginManager from './Componentes/Login/LoginManager';
 
 import { setuserName } from './Redux/store'
@@ -23,88 +22,3 @@ export default class App extends Component {
     );
   }
 }
-=======
-import Login from './Componentes/Login/Login'
-
-
-
-export default class App extends Component {
-
-  render () {
-    return (
-        <MyApp/>     
-    );
-  }
-}
-
-class Home extends Component {
-  render(){
-    return (
-      <Text style={{marginTop: StatusBar.currentHeight}}>Administrar Usuarios</Text>
-    )
-  }
-}
-class Lote extends Component {
-  render(){
-    return (
-      <Text style={{marginTop: StatusBar.currentHeight}}>Administrar Lote</Text>
-    )
-  }
-}
-
-class AuthLoading extends Component{
-
-  componentDidMount(){
-    const userToken = false;
-    this.props.navigation.navigate(userToken ? 'App' : 'Auth');
-  }
-
-  render(){
-    return(
-      <View>
-        <ActivityIndicator />
-        <StatusBar barStyle="default" />
-      </View>
-    )
-  }
-}
-
-
-
-const DrawerNavigation = createDrawerNavigator({
-    Usuarios: {
-      screen: Home
-    },
-    RegistrarLote:{
-      screen: Lote
-    }
-});
-
-const SwitchLogin = createSwitchNavigator({
-  AuthLoading: {
-    screen: AuthLoading
-  },
-  App: {
-    screen: DrawerNavigation
-  },
-  Auth:{
-   screen: Login 
-  }
-},{
-  initialRouteName: 'AuthLoading'
-})
-
-const MyApp = createAppContainer(SwitchLogin);
-
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
->>>>>>> Stashed changes
--- Componentes/Main/Notificaciones.js
+++ Componentes/Main/Notificaciones.js
@@ -4,24 +4,17 @@ import {View, Text, StyleSheet} from 'react-native'
 import Ionicons from '@expo/vector-icons/Ionicons'
 import { Avatar, Badge, Icon, withBadge } from 'react-native-elements'
 
+
+
 class Notificaciones extends Component {
 
 
-    BadgedIcon = withBadge(1)(Icon);
+ 
 
     render() {
         return (
-            <View style={{flexDirection: 'row', justifyContent:'center',flexWrap:'wrap'}}>
-                <View style={{width:'50%'}}>
-                    <Ionicons
-                        name="ios-person"
-                        size={30}
-                    />
-                </View>
-                <View style={{width:'50%'}}>
-                
-                <BadgedIcon type="ionicon" name="ios-chatbubbles" />
-                </View>
+            <View style={{width:'100%',flexDirection: 'row', justifyContent:'center',flexWrap:'wrap', backgroundColor:'red'}}>
+              
             </View>
         );
     }
